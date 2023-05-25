c0f64c48e34e91ed5b1cb4591577a4f61c0899c7
Erik <eriking1503@gmail.com>
2023-05-25 16:35:50 +0300

    Revert "Sprites & prefabs changes"
    This reverts commit a6a4b1f1c4896dd49e90fde38dde7be43c2739c4.

---
+++ Farm Game/Assets/Scripts/ToolsCharacterController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolsCharacterController : MonoBehaviour
+{
+    private CharacterController2D character;
+    private Rigidbody2D rgbd2d;
+    [SerializeField] private float offsetDistance = 1f;
+    [SerializeField] private float sizeOfInteractableArea = 1.2f;
+    // Toolbar_UI
+    public Player player;
+    public ItemData item;
+
+    private void Awake()
+    {
+        character = GetComponent<CharacterController2D>();
+        rgbd2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            UseToolWorld();
+        }
+    }
+    
+
+    private bool UseToolWorld()
+    {
+        Vector2 position = rgbd2d.position;
+        if (player.inventory.toolbar.slots[GameManager.instance.selectedItemIndex].itemName == "Axe")
+        {
+            item.itemName = "Axe";
+        }
+        
+        if (player.inventory.toolbar.slots[GameManager.instance.selectedItemIndex].itemName == "Pickaxe")
+        {
+            item.itemName = "Pickaxe";
+        }
+
+        bool complete = item.onAction.OnApply(position);
+        
+        return complete;
+    }
+
+}
